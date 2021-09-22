7bb4b41a6fdaa990851a26e84c06ea193e4e7718
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2021-09-22 11:05:32 +0200

    Timespan: setDefault before setting hideIf

--- classes/form/search_form.php
+++ classes/form/search_form.php
@@ -72,13 +72,13 @@ class search_form extends moodleform {
 
         $mform->addElement('date_time_selector', 'sfcoursestarttime', get_string('sfcoursestarttime', 'block_booking'));
         $mform->setType('sfcoursestarttime', PARAM_INT);
-        $mform->hideIf('sfcoursestarttime', 'sftimespancheckbox');
         $mform->setDefault('sfcoursestarttime', strtotime(date('Y-m-d') . ' 00:00'));
+        $mform->hideIf('sfcoursestarttime', 'sftimespancheckbox');
 
         $mform->addElement('date_time_selector', 'sfcourseendtime', get_string('sfcourseendtime', 'block_booking'));
         $mform->setType('sfcourseendtime', PARAM_INT);
-        $mform->hideIf('sfcourseendtime', 'sftimespancheckbox');
         $mform->setDefault('sfcourseendtime', strtotime(date('Y-m-d') . ' 23:59'));
+        $mform->hideIf('sfcourseendtime', 'sftimespancheckbox');
 
         $this->add_action_buttons(false, get_string('sfsearchbtn', 'block_booking'));
     }
