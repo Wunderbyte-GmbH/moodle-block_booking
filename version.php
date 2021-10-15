668bba3abb622409a8ecda5fad9195c42bc1d285
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2021-10-15 12:43:20 +0200

    Version 3.0.3 (2021101500)

--- version.php
+++ version.php
@@ -26,12 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021100400; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release = '3.0.2';
+$plugin->version = 2021101500; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = '3.0.3';
 $plugin->component = 'block_booking';
 $plugin->cron = 0;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires  = 2019111800;
 $plugin->dependencies = array(
-                'mod_booking' => 2021100400
+                'mod_booking' => 2021101500
 );
